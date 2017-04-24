b80cf4c3c5af537fd2d5523965c0142ecd587184
Cosmo <hackjutsu@users.noreply.github.com>
2017-04-23 18:53:34 -0700

    Update the Pokemon data refresh interval to 1s

--- myMap.js
+++ myMap.js
@@ -98,5 +98,5 @@ function refresh_pokemon_data() {
     });
 }
 
-window.setInterval(refresh_pokemon_data, 5000);
+window.setInterval(refresh_pokemon_data, 1000);
 window.setInterval(refresh_pokemon_layer, 1000);
